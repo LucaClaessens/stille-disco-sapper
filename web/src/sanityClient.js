adc50dcf91ffe43166d0d2a70aea878206e2ffca
Luca Claessens <lucaclae@gmail.com>
2021-02-21 20:03:01 +0100

    updated deploy contexts in netliy.toml file

--- web/src/sanityClient.js
+++ web/src/sanityClient.js
@@ -1,10 +1,11 @@
-import sanityClient from '@sanity/client'
-import { api } from '../../studio/sanity.json'
-const { projectId, dataset } = api
+import sanityClient from '@sanity/client';
+import { api } from '../../studio/sanity.json';
+const { projectId } = api;
+const { SANITY_STUDIO_API_DATASET } = process.env;
 
 const client = sanityClient({
   projectId,
-  dataset,
+  dataset: SANITY_STUDIO_API_DATASET,
   token: "skIUpdCGXRCcJ2QQaMGUHL6eG88ynOIQNzUmU1FTZgyMTDItuJeL4c7WdsG6Qd61dxqQ6svUMOk54kAwaVudL1DFFlaqIhf2Gvv75tX5cPr8wIcYAWlhM1P8aXNWRzQEoUvLVa9RohU9VyUTH38EA7dWqhe32Atcwu1JTu0hMkmidVigpzk5",
   useCdn: true
 })
