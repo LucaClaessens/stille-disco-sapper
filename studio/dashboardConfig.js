32d8d5ca32256527359c4fe32b6f4b1d060be28e
Luca Claessens <lucaclae@gmail.com>
2021-02-21 16:36:56 +0100

    updated build hooks in dashboard of test env

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -11,14 +11,14 @@ export default {
                 'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
               sites: [
                 {
-                  buildHookId: '5ff471e90ca292008b5b9c10',
-                  title: 'Sanity Studio',
+                  buildHookId: '60327cf0f3ce85967ba01e48',
+                  title: 'Sanity studio (dev)',
                   name: 'stille-disco-sapper-studio',
                   apiId: '05bf6169-557f-4c38-9d9c-1dd2f7e631bc'
                 },
                 {
-                  buildHookId: '5ff471e938a80e00977c64ea',
-                  title: 'Stille Disco Websites',
+                  buildHookId: '60327de8b2938b9b27847068',
+                  title: 'Stille Disco (dev)',
                   name: 'stille-disco-sapper',
                   apiId: '256c76ae-1e9a-49ee-87ec-7f63d07b35f8'
                 }
