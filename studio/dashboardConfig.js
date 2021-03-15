4dfed6023aa24afd77cbd1218f7728a6ae648a0d
Luca Claessens <lucaclae@gmail.com>
2021-03-15 20:36:04 +0100

    move build hooks into a configuration file

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -1,3 +1,5 @@
+import buildHooks from "./config/buildHooks";
+
 export default {
   widgets: [
     {
@@ -9,20 +11,7 @@ export default {
             options: {
               description:
                 'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
-              sites: [
-                {
-                  buildHookId: '60327cf0f3ce85967ba01e48',
-                  title: 'Sanity studio (dev)',
-                  name: 'stille-disco-sapper-studio',
-                  apiId: '05bf6169-557f-4c38-9d9c-1dd2f7e631bc'
-                },
-                {
-                  buildHookId: '60327de8b2938b9b27847068',
-                  title: 'Stille Disco (dev)',
-                  name: 'stille-disco-sapper',
-                  apiId: '256c76ae-1e9a-49ee-87ec-7f63d07b35f8'
-                }
-              ]
+              sites: buildHooks
             }
           }
         ],
