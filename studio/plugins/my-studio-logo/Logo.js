8c661cacb52f4c19b68ed8f5664aaec5f9b3f265
Luca Claessens <lucaclae@gmail.com>
2021-04-13 19:01:24 +0200

    update studio logo component

--- studio/plugins/my-studio-logo/Logo.js
+++ studio/plugins/my-studio-logo/Logo.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import styles from './Logo.module.css';
 
 const dataset = process.env.SANITY_STUDIO_API_DATASET
 
 const Logo = () => {
   if (dataset === "development") {
     return (
-      <div>DEVELOPMENT</div>
+      <div className={styles.container}>
+        <img src="/static/logo-192.png" alt="Stille disco logo"></img>
+        <div className={styles.burst}>
+        <p>DEV</p>
+        </div>
+      </div>
     )
   }
   return (
-    <div>STILLE DISCO LOGO</div>
+    <div className={styles.container}>
+    <img src="/static/logo-192.png" alt="Stille disco logo" ></img>
+  </div>
   )
 }
 
