5dbf88a449a8e60c30598a0b1e6a7ea50e59c7ca
Luca Claessens <lucaclae@gmail.com>
2021-04-13 19:38:51 +0200

    query events from current moment onwards

--- functions/events.js
+++ functions/events.js
@@ -1,51 +1,47 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 const stager = {
-    token: process.env.STAGER_TOKEN,
+  token: process.env.STAGER_TOKEN,
 };
 
 const fetchEvents = async (currentIndex = 0) => {
-    const endpoint = `https://feeds.stager.nl/events?pageIndex=${currentIndex}`;
+  const now = new Date().toISOString();
+  const endpoint = `https://feeds.stager.nl/events?pageIndex=${currentIndex}&from=${now}`;
 
-    const response
-        = await fetch(endpoint, {
-            method: 'GET',
-            headers: {
-                'accept': 'application/json;version=V1',
-                'Authorization': `Bearer ${stager.token}`
-            },
-        }).then(res => res.json());
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: {
+      accept: "application/json;version=V1",
+      Authorization: `Bearer ${stager.token}`,
+    },
+  }).then((res) => res.json());
 
-    const { pageIndex, lastPageIndex, events } = response;
+  const { pageIndex, lastPageIndex, events } = response;
 
-    if (pageIndex < lastPageIndex) {
-        return [...events, await fetchEvents(pageIndex)]
-    } else {
-        return events;
-    }
-}
-
+  if (pageIndex < lastPageIndex) {
+    return [...events, await fetchEvents(pageIndex)];
+  } else {
+    return events;
+  }
+};
 
 exports.handler = async function (event) {
-    if (event.httpMethod === 'GET') {
-        try {
-            return {
-                statusCode: 200,
-                body: JSON.stringify(await fetchEvents())
-            };
-
-        } catch (err) {
-            return {
-                statusCode: 500,
-                body: JSON.stringify(err)
-            };
-        }
-    } else {
-        return {
-            statusCode: 405,
-            body: JSON.stringify({ err: "Method not allowed" })
-        };
+  if (event.httpMethod === "GET") {
+    try {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(await fetchEvents()),
+      };
+    } catch (err) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err),
+      };
     }
-
-
-}
+  } else {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ err: "Method not allowed" }),
+    };
+  }
+};
