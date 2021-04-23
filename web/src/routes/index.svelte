0877464cc54cb62dc76ef104d027167c0518a9cb
Luca Claessens <lucaclae@gmail.com>
2021-04-23 09:30:22 +0200

    trap on landing temporarily

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,35 +1,13 @@
 <script>
-  import { goto } from "@sapper/app";
-  import { onMount } from "svelte";
-  import { fade } from "svelte/transition";
-  import projectLanguage from "../utils/i18n/projectLanguage";
-  import { languages } from "./../../../studio/schemas/languages";
-
-  const languageParams = languages.map((lang) =>
-    projectLanguage(lang.name, true)
-  );
-
-  onMount(async () => {
-    const navigatorLanguage = navigator.language;
-    const parsedLanguage = projectLanguage(
-      navigatorLanguage.replace("-", "_"),
-      true
-    );
-    await goto("/" + parsedLanguage);
-  });
 </script>
 
 <svelte:head>
-  <title>Stille disco</title>
+  <title>Stille disco, coming soon</title>
 </svelte:head>
-
-{#each languageParams as param}
-  <a href={param} class="hidden">{param}</a>
-{/each}
 
 <div
   transition:fade
   class="bg-black w-full h-full flex items-center justify-center"
 >
-  <p class="text-white">🙂</p>
+  <p class="text-white">NEW WEBSITE COMING SOON!</p>
 </div>
