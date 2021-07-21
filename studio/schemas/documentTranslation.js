650ea1ce3638c9d859f311d5875317e79a27cbe9
Luca Claessens <lucaclae@gmail.com>
2021-07-21 15:53:31 +0200

    translation update

--- studio/schemas/documentTranslation.js
+++ studio/schemas/documentTranslation.js
@@ -3,42 +3,44 @@ import { baseLanguage, languages } from './languages'
 // This is configuration for whole-document translations via sanity-intl plugin
 // See https://github.com/LiamMartens/sanity-plugin-intl-input/blob/master/docs/general-configuration.md
 export const i18n = {
-    base: baseLanguage.name,
-    languages,
-    messages: {
-        loading: 'Loading languages...',
-        missing: 'Missing',
-        draft: 'Draft',
-        publishing: 'Publishing...',
-        publish: 'Publish',
-        deleteAll: {
-            buttonTitle: 'Delete (incl. translations)',
-            deleting: 'Deleting...',
-        },
-        translationsMaintenance: {
-            title: 'Translation Maintenance',
-            selectSchemaPlaceholder: 'Select schema type',
-            oldIdStructure: 'document(s) are using the old ID structure',
-            missingLanguageField: 'document(s) are missing the language field',
-            missingDocumentRefs: 'document(s) have missing translation references',
-            orphanDocuments: 'orphaned translation document(s)',
-            fix: 'Fix'
-        },
+  base: baseLanguage.name,
+  languages,
+  referenceBehavior: 'weak',
+  messages: {
+    loading: 'Loading languages...',
+    missing: 'Missing',
+    draft: 'Draft',
+    publishing: 'Publishing...',
+    publish: 'Publish',
+    deleteAll: {
+      buttonTitle: 'Delete (incl. translations)',
+      deleting: 'Deleting...'
     },
-    fieldNames: {
-        lang: '_lang',
-        references: '_langRefs'
-    },
-    fields: []
+    translationsMaintenance: {
+      title: 'Translation Maintenance',
+      selectSchemaPlaceholder: 'Select schema type',
+      oldIdStructure: 'document(s) are using the old ID structure',
+      missingLanguageField: 'document(s) are missing the language field',
+      missingDocumentRefs: 'document(s) have missing translation references',
+      orphanDocuments: 'orphaned translation document(s)',
+      fix: 'Fix'
+    }
+  },
+  fieldNames: {
+    lang: '_lang',
+    references: '_langRefs'
+  },
+  fields: []
 }
 
 export const i18nOptions = {
-    i18n: true,
-    base: baseLanguage.name,
-    languages,
-    css: (classNames) => `${classNames} custom-classname`,
-    messages: {
-        loading: 'Loading languages...',
-        missingTranslations: 'Missing translations message...',
-    },
+  i18n: true,
+  base: baseLanguage.name,
+  languages,
+  referenceBehavior: 'weak',
+  css: classNames => `${classNames} custom-classname`,
+  messages: {
+    loading: 'Loading languages...',
+    missingTranslations: 'Missing translations message...'
+  }
 }
